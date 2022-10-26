78f70893f3090e455c2b987c3448f14dd8d3eb77
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-10-25 17:01:30 -0700

    added results.js page

--- assets/results.js
+++ assets/results.js
@@ -1,8 +1,31 @@
 // constant for player info
 
-const unsername = document.querySelector('#username');
+const username = document.querySelector('#username');
 const saveScore = document.querySelector('#finalscore');
 const finalScore = document.querySelector('#finallScore');
 const recentScore = document.querySelector('#recentScore');
 
-//   loggint score to console
+//   loggin score to console
+finalScore.innerText = recentScore;
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const Max_High_Score = 5;
+
+username.addEventListener('keydown', () => {
+    saveScore.disabled = !username.value;
+});
+
+saveHighScore =e => {
+    e.preventDefault();
+    
+    const score = {
+        name: username.value,
+        score: finalScore.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => {
+    return a.score - b.score;
+})
+highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    }
